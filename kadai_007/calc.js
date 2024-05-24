f4c0a38d11dfa30a180c0297e74fb2037ef67e7d
chieko117 <165668186+chieko117@users.noreply.github.com>
2024-05-24 15:10:15 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,14 @@
+let num = 15;
+
+if (15 % 3 === 0 && 15 % 5 === 0) {
+    console.log('3と5の倍数です');
+}
+else if (15 % 3 === 0) { 
+    console.log('3の倍数です');
+}
+else if (15 % 5 === 0) {
+    console.log('5の倍数です');
+}
+else { 
+    console.log('num');
+}
