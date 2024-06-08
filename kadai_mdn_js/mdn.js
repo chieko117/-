b218f9af7ea5b8568554f23ebc7c866926f7247c
chieko117 <165668186+chieko117@users.noreply.github.com>
2024-06-08 09:59:56 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,8 @@
+var date = new Date();
+console.log(date); 
+// 「年」要素を取得
+console.log(date.getFullYear() + '年'); 
+// 「月」要素を取得
+console.log((date.getMonth() + 1) + '月'); 
+// 「日」要素を取得
+console.log(date.getDate() + '日'); 
